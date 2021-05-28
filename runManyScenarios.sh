6818e046f445a0f3d645982e7a31731d9ae91802
Tiegan Hobbs <59665171+tieganh@users.noreply.github.com>
2021-05-28 16:21:33 -0700

    Loading Sidney scenarios

---
+++ runManyScenarios.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#MODELS TO RUN:
+sh scripts/run_OQStandard.sh IDM4p7_Sidney
+sh scripts/run_OQStandard.sh IDM4p9_Sidney
+sh scripts/run_OQStandard.sh IDM5p1_Sidney
+sh scripts/run_OQStandard.sh IDM5p3_Sidney
+sh scripts/run_OQStandard.sh IDM5p5_Sidney
+sh scripts/run_OQStandard.sh IDM5p7_Sidney
+sh scripts/run_OQStandard.sh IDM5p9_Sidney
+sh scripts/run_OQStandard.sh IDM6p1_Sidney
+sh scripts/run_OQStandard.sh IDM6p3_Sidney
+sh scripts/run_OQStandard.sh IDM6p5_Sidney
+sh scripts/run_OQStandard.sh IDM6p7_Sidney
+sh scripts/run_OQStandard.sh IDM6p9_Sidney
+sh scripts/run_OQStandard.sh IDM7p1_Sidney
+
+

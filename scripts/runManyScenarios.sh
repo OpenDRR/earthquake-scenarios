67c31dbf4f3ee81100b92b719e88f455c0b1c6d4
Tiegan Hobbs <59665171+tieganh@users.noreply.github.com>
2021-05-28 11:58:55 -0700

    updating attribution in readme and adding multirun script

---
+++ scripts/runManyScenarios.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#MODELS TO RUN:
+sh ../scripts/run_OQStandard.sh SIM9p0_CSZlockedtrans
+sh ../scripts/run_OQStandard.sh IDM7p1_JdF
+sh ../scripts/run_OQStandard.sh SCM7p1_Charlevoix
+sh ../scripts/run_OQStandard.sh AFM7p3_GSM
+sh ../scripts/run_OQStandard.sh IDM6p8_JdF
